23994f30e59abac102181c5968d2c69ae5867a55
Julio Xavier <julioxavierr@live.com>
2018-03-02 10:38:27 -0300

    React Navigation

--- App.js
+++ App.js
@@ -1,68 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { StyleSheet, FlatList, View, Text, TouchableHighlight } from 'react-native';
-import { upperCase } from 'change-case';
+import { StackNavigator } from 'react-navigation';
+import PostList from './PostList';
+import PostDetail from './PostDetail';
 
-export default class App extends React.Component {
+RootStack = StackNavigator(
+    {
+        List: {screen: PostList},
+        Detail: {screen: PostDetail}
+    },
+    {
+        initialRouteName: 'List'
+    }
+);
 
-  /**
-   * Render a row
-   * @param {object} rowData 
-   */
-  _renderRowView(rowData) {
-    return (<View>
-            <TouchableHighlight style={styles.row} 
-              underlayColor='#EEE'
-              onPress={() => console.log(rowData.node.description + ' pressed')}>
-              <Text>{rowData.node.description}</Text>
-            </TouchableHighlight>
-            <View style={styles.line} />
-            </View>)
-  }
-  
-  render() {
-    mockData = [
-      {
-        node: {
-          id: 1,
-          description: 'Post 1'
-        }
-      },
-      {
-        node: {
-          id: 2,
-          description: 'Post 2'
-        }
-      }
-    ];
-
-    return (
-      <View style={styles.container}>
-        <View style={styles.navbar}/>
-        <FlatList
-          data={mockData}
-          keyExtractor={(item) => item.node.id}
-          renderItem={({item}) => this._renderRowView(item)}
-        />
-      </View>
-    );
-  }
+export default class App extends Component {
+    render() {
+        return <RootStack/>;
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-   flex: 1,
-   backgroundColor: '#FFF'
-  },
-  navbar: {
-    height: 64,
-    backgroundColor: '#51103D'
-  },
-  row: {
-    padding: 10,
-    height: 44
-  },
-  line: {
-    backgroundColor:'#CCC',
-    height: 1
-  },
-})
