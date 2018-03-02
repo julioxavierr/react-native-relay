97c43347d0b77d719c3c3be90f66e426088bd740
Julio Xavier <julioxavierr@live.com>
2018-03-01 23:28:58 -0300

    Flatlist with mocked data

--- App.js
+++ App.js
@@ -2,22 +2,29 @@ import React from 'react';
 import { StyleSheet, FlatList, View, Text} from 'react-native';
 
 export default class App extends React.Component {
-
+  
   render() {
+    mockData = [
+      {
+        node: {
+          id: 1,
+          description: 'Post 1'
+        }
+      },
+      {
+        node: {
+          id: 2,
+          description: 'Post 2'
+        }
+      }
+    ];
+
     return (
       <View style={styles.container}>
         <FlatList
-          data={[
-            {key: 'Post1'},
-            {key: 'Post2'},
-            {key: 'Post3'},
-            {key: 'Post4'},
-            {key: 'Post5'},
-            {key: 'Post6'},
-            {key: 'Post7'},
-            {key: 'Post8'},
-          ]}
-          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+          data={mockData}
+          keyExtractor={(item) => item.node.id}
+          renderItem={({item}) => <Text style={styles.item}>{item.node.description}</Text>}
         />
       </View>
     );
