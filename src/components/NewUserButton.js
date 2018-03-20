587dfd7e54c6f00c575ab526bd180c932bb24e79
Julio Xavier <julioxavierr@gmail.com>
2018-03-20 15:43:12 -0300

    Using onPress prop properly

--- src/components/NewUserButton.js
+++ src/components/NewUserButton.js
@@ -1,15 +1,17 @@
+// @flow
+
 import React, { Component } from 'react';
 import { Text, TouchableHighlight } from 'react-native';
 
 export default class NewUserButton extends Component {
 
-    _onPress = () => {
-        this.props.navigation.navigate('New', {});
+    _onPress = (navigation) => {
+        navigation.navigate('New', {});
     }
 
     render() {
         return (
-            <TouchableHighlight style={style} onPress={() => this._onPress(this.navigation)}>
+            <TouchableHighlight style={style} onPress={() => this._onPress(this.props.navigation)}>
                     <Text style={{fontSize: 30, color: '#FFF'}}>New user</Text>
             </TouchableHighlight>
         )
