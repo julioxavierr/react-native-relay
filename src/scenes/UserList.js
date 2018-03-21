b85137d85c2045a59359ff93181bea46383618e6
Julio Xavier <julioxavierr@gmail.com>
2018-03-20 22:01:32 -0300

    UserList subscription method

--- src/scenes/UserList.js
+++ src/scenes/UserList.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { StyleSheet, FlatList, View, Text, TouchableHighlight } from 'react-native';
 import { withNavigation } from 'react-navigation';
-import { createFragmentContainer, graphql, QueryRenderer } from 'react-relay';
+import { createFragmentContainer, graphql, QueryRenderer, requestSubscription } from 'react-relay';
 import hoistStatics from 'hoist-non-react-statics';
 import environment from '@src/Environment';
 import RowItem from '@src/components/RowItem';
@@ -11,8 +11,20 @@ import NewUserButton from '@src/components/NewUserButton';
 @withNavigation
 class UserList extends Component {
 
-
     static navigationOptions = {title: 'List'};
+
+    _subscribe = () => {
+        requestSubscription(
+            environment,
+            {
+                subscription,
+                variables: {},
+                onCompleted: () => console.log('Subscription completed'),
+                updater: (proxyStore) => {
+                    console.log('Subscription updater')
+                }
+        });
+    }
 
     // Render a view including a FlatList that contains RowItem's 
     render() {
@@ -32,6 +44,22 @@ class UserList extends Component {
 
 }
 
+const subscription = graphql`
+    subscription UserListSubscription {
+        UserAdded {
+            userEdge {
+                node {
+                    id
+                    name
+                    email
+                    description
+                    imageUrl
+                }
+            }
+        }
+    }
+`;
+
 // Get the last 20 users names using GraphQL
 // and provide info to User_user
 const UserListContainer = createFragmentContainer(UserList, graphql`
@@ -50,13 +78,13 @@ const UserListContainer = createFragmentContainer(UserList, graphql`
 const UserListQueryRenderer = () => {
     return (<QueryRenderer environment={environment}
         query={graphql`
-            query UserListQuery{
+            query UserListQuery {
                 ...UserList_query
             }
         `}
         render={({error, props}) => {
             if (error) {
-                return <Text>Error...</Text>
+                return <Text>{error}</Text>
             } else if (props) {
                 // Expected path
                 return <UserListContainer query={props}/>
