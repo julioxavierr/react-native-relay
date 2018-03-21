6ceeb41d224cd67fa5e4d4103d8ef55af422d49a
Julio Xavier <julioxavierr@gmail.com>
2018-03-20 22:46:42 -0300

    New environment

--- src/Environment.js
+++ src/Environment.js
@@ -1,15 +1,41 @@
-const {
-    Environment,
-    Network,
-    RecordSource,
-    Store,
-  } = require('relay-runtime');
+import {Environment, Network, RecordSource, Store} from 'relay-runtime';
+import {SubscriptionClient} from 'subscriptions-transport-ws'
+import 'whatwg-fetch'
 
   const store = new Store(new RecordSource());
 
+  const setupSubscription = (config, variables, cacheConfig, observer) => {
+    const query = config.text
+    const subscriptionClient = new SubscriptionClient('ws://localhost:5000/subscriptions', {reconnect: true})
+    console.log("subscriptionClient = ", subscriptionClient)
+    const onNext = (result) => {
+      observer.onNext(result)
+    }
+    const onError = (error) => {
+      observer.onError(error)
+    }
+    const onComplete = () => {
+      observer.onCompleted()
+    }
+  
+    const client = subscriptionClient
+      .request({query, variables})
+      .subscribe(onNext, onError, onComplete)
+  
+    // Return a dispose method to be able to unsubscribe and trigger closing the
+    // socket connection
+    return {
+      dispose: () => {
+        // unsubscribe and close this socket connection
+        client.unsubscribe()
+        subscriptionClient.close()
+      }
+    }
+  }
+
   const network = Network.create((operation, variables) => {
 
-    return fetch('http://localhost:5000/graphiql', {
+    return fetch('http://localhost:5000/graphql', {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
@@ -22,7 +48,7 @@ const {
     }).then(response => {
       return response.json()
     });
-  });
+  }, setupSubscription);
 
   const environment = new Environment({
     network,
