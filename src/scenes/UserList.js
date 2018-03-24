e8a4c47310fb611c132ac955750200e17d4e44cd
Julio Xavier <julioxavierr@gmail.com>
2018-03-24 00:38:41 -0300

    Subscription separated from scene. This closes #6.

--- src/scenes/UserList.js
+++ src/scenes/UserList.js
@@ -1,126 +1,94 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import { withNavigation } from 'react-navigation';
-import { createFragmentContainer, graphql, QueryRenderer, requestSubscription } from 'react-relay';
+import { createFragmentContainer, graphql, QueryRenderer } from 'react-relay';
 import hoistStatics from 'hoist-non-react-statics';
 import environment from '@src/Environment';
 import RowItem from '@src/components/RowItem';
 import BpkSpinner from 'react-native-bpk-component-spinner';
 import NewUserButton from '@src/components/NewUserButton';
-import { ConnectionHandler } from 'relay-runtime';
-import styled from 'styled-components'
+import styled from 'styled-components';
 import Wrapper from '@src/components/Wrapper';
+import subscribe from '../subscriptions/UserSubscription';
 
 @withNavigation
 class UserList extends Component {
+  componentDidMount() {
+    subscribe();
+  }
 
-    componentDidMount() {
-        // this._subscribe();
-    }
+  static navigationOptions = { title: 'List' };
 
-    static navigationOptions = { title: 'List' };
+  // Render a view including a FlatList that contains RowItem's
+  render() {
+    this.navigation = this.props.navigation;
+    const users = this.props.query.users;
 
-    _subscribe = () => {
-        requestSubscription(
-            environment,
-            {
-                subscription,
-                variables: {},
-                updater: (store) => {
+    const rowItem = item => (
+      <RowItem data={item} navigation={this.navigation} />
+    );
 
-                    // Get the notification
-                    const rootField = store.getRootField('UserAdded');
-                    const newUser = rootField.getLinkedRecord('userEdge').getLinkedRecord('node');
-
-                    // Add it to a connection
-                    const record = store.getRoot();
-                    const users = ConnectionHandler.getConnection(record, 'UserList_users');
-
-                    if(users) {
-                        const newEdge = ConnectionHandler.createEdge(store, users, newUser, 'UserEdge');
-                        ConnectionHandler.insertEdgeBefore(users, newEdge);
-                    }
-
-                },
-        });
-    }
-
-    // Render a view including a FlatList that contains RowItem's 
-    render() {
-        this.navigation = this.props.navigation;
-        const users = this.props.query.users;
-
-        const rowItem = (item) => <RowItem data={item} navigation={this.navigation}/>;
-
-        return (
-            <Wrapper>
-                <List data={users.edges} keyExtractor={(item) => item.node.id}
-                    renderItem={({ item }) => rowItem(item)}
-                />
-                <NewUserButton navigation={this.navigation}/>
-            </Wrapper>
-        );
-    }
-
+    return (
+      <Wrapper>
+        <List
+          data={users.edges}
+          keyExtractor={item => item.node.id}
+          renderItem={({ item }) => rowItem(item)}
+        />
+        <NewUserButton navigation={this.navigation} />
+      </Wrapper>
+    );
+  }
 }
-
-const subscription = graphql`
-    subscription UserListSubscription {
-        UserAdded {
-            userEdge {
-                node {
-                    id
-                    name
-                    email
-                    description
-                    imageUrl
-                }
-            }
-        }
-    }
-`;
 
 // Get the last 20 users names using GraphQL
 // and provide info to User_user
-const UserListContainer = createFragmentContainer(UserList, graphql`
+const UserListContainer = createFragmentContainer(
+  UserList,
+  graphql`
     fragment UserList_query on Query {
-        users(last: 20) @connection(key: "UserList_users") {
-            edges {
-                node {
-                    id
-                    name
-                }
-            }
+      users(last: 20) @connection(key: "UserList_users") {
+        edges {
+          node {
+            id
+            name
+          }
         }
+      }
     }
-`)
+  `,
+);
 
 const UserListQueryRenderer = () => {
-    return (<QueryRenderer environment={environment}
-        query={graphql`
-            query UserListQuery {
-                ...UserList_query
-            }
-        `}
-        render={({ error, props }) => {
-            if (error) {
-                return <View>{error}</View>
-            } else if (props) {
-                // Expected path
-                return <UserListContainer query={props}/>
-            } else {
-                // Display loading spinner
-                return (
-                    <Wrapper>
-                        <BpkSpinner type="light" />
-                    </Wrapper>
-                )
-            }
-        }}/>)
-}
+  return (
+    <QueryRenderer
+      environment={environment}
+      query={graphql`
+        query UserListQuery {
+          ...UserList_query
+        }
+      `}
+      render={({ error, props }) => {
+        if (error) {
+          return <View>{error}</View>;
+        } else if (props) {
+          // Expected path
+          return <UserListContainer query={props} />;
+        } else {
+          // Display loading spinner
+          return (
+            <Wrapper>
+              <BpkSpinner type="light" />
+            </Wrapper>
+          );
+        }
+      }}
+    />
+  );
+};
 
 export default hoistStatics(UserListQueryRenderer, UserList);
 
 const List = styled.FlatList`
-    flex: 1;
-`
+  flex: 1;
+`;
